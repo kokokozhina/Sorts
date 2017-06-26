71bb5c3d4618fcb52e76d46331d0c337174ffaa6
kokokozhina <kozhina.olga.1997@gmail.com>
2017-06-26 09:44:08 +0400

    Add files via upload

--- timsort.c
+++ timsort.c
@@ -1,21 +1,28 @@
-// #define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <malloc.h>
 #include <time.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+#include <limits.h>
 #include <string.h>
-//using https://neerc.ifmo.ru/wiki/index.php?title=Timsort
+#include <float.h>
+
+#define min(x,y) ((x) < (y) ? (x) : (y))
+#define max(x,y) ((x) > (y) ? (x) : (y))
+
 struct pair {
-	int first;
-	int second;
+	long long start;
+	long long length;
 };
+
+FILE *in, *out;
 
 //run - subarray of input array
 //run-array can have a length bigger then minrun 
 //minrun - minimal size of run-subarray
-int getMinrun(int n)
+inline int getMinrun(int n)
 { //getting the minimal size of run
 	int r = 0;  // becomes 1 if the least significant bits contain at least one off bit 
 	while (n >= 64) {
@@ -25,7 +32,7 @@ int getMinrun(int n)
 	return n + r;
 }
 
-void reverse(int *a, int lo, int hi)
+inline void reverse(int *a, int lo, int hi)
 { //reversing a array in [lo, hi)
 	int upper_bound = lo + (hi - lo) / 2;
 	int temp;
@@ -38,59 +45,31 @@ void reverse(int *a, int lo, int hi)
 }
 
 int getRun(int *a, int i, int n, int minrun)
-{ //returns the end of subarray (sorted in ascending order) with length >= minrun and beginning at i
-	int j = i + 1;
-	bool order = true; //ascending = 1, descending = 0;
-	while (j < n && j - i < minrun)
+{
+	bool order;
+	int j;
+	if (n - i <= 2)
 	{
-		if (n - j >= 2)
+		if (n - i == 2 && a[i] > a[i + 1])
 		{
-			j++;
-			order = a[i] <= a[j - 1] ? true : false;
-			while (j < n && ((order && a[j - 1] <= a[j]) || (!order  && a[j - 1] >= a[j])))
-				j++;
-			if (!order)
-				reverse(a, i, j);
+			int temp = a[i];
+			a[i] = a[i + 1];
+			a[i + 1] = temp;
 		}
-		else
-			j++;
+		return n;
 	}
-	return j;
+	order = a[i] > a[i + 1] ? true : false;
+	j = i + 2;
+	while (j < n && ((!order && a[j - 1] <= a[j]) || (order && a[j - 1] >= a[j])))
+		j++;
+	if (order)
+		reverse(a, i, j);
+	i = j;
+
+	return i;
 }
 
-void insertionSort(int *a, int n, int lo, int hi) //Kormen, 2013, page 48
-{ //used for sorting runs (subarray with length >= minrun)
-	for (int j = lo + 1; j < hi; j++)
-	{
-		int key = a[j];
-		int i = j - 1;
-		while (i >= lo && a[i] > key)
-		{
-			a[i + 1] = a[i];
-			i--;
-		}
-		a[i + 1] = key;
-	}
-}
-
-void partition(int *a, int n, int minrun, struct pair *stack, int *sp)
-{ //diving input array into sorted runs and pushing them in stack
-  //sp = StackPointer - points on the first unused cell
-	int i = 0, j = 1;
-	struct pair subarray;
-	while (i < n)
-	{
-		j = getRun(a, i, n, minrun);
-		insertionSort(a, n, i, j);
-		subarray.first = i;
-		subarray.second = j - i;
-		(*sp)++;
-		stack[*sp - 1] = subarray;
-		i = j;
-	}
-}
-
-int binsearch(int *aux, int start, int m, int search)
+int binsearch(int *a, int start, int m, int search)
 {
 	int l = start;
 	int r = m - 1;
@@ -98,162 +77,406 @@ int binsearch(int *aux, int start, int m, int search)
 	while (r - l > 1)
 	{
 		mid = l + ((r - l) >> 1);
-		if (aux[mid] <= search)
+		if (a[mid] <= search)
 			l = mid;
 		else
 			r = mid;
 	}
-	if (aux[l] > search)
+	if (a[l] > search)
 		return l;
 	else
 		return r;
 }
 
+void insertionSort(int *a, int lo, int hi)
+{
+	int pos, x;
+	for (int i = lo + 1; i < hi; i++)
+	{
+		if (a[i - 1] <= a[i])
+			continue;
+		x = a[i];
+		pos = binsearch(a, lo, i, x);
+		for (int j = i - 1; j >= pos; j--)
+			a[j + 1] = a[j];
+		a[pos] = x;
+	}
+
+}
+
+void gallop(int *a, int beg, int end, int num, int x, bool dir, int *ans)
+{
+	if (!dir)
+	{
+		// fprintf(out, "IN asc GALLOP\n");
+		if (beg + num - 1 < end && a[beg + num - 1] <= x)
+			gallop(a, beg, end, min(end - beg + 1, 2 * num), x, dir, ans);
+		if (*ans == -100500)
+			*ans = binsearch(a, max(beg, beg + num / 2 - 1), min(end, beg + num - 1) + 1, x);
+
+	}
+	else
+	{
+		// fprintf(out, "IN desc GALLOP\n");
+		if (end - num + 1 > beg && a[end - num + 1] >= x)
+			gallop(a, beg, end, min(end - beg + 1, num * 2), x, dir, ans);
+		if (*ans == -100500)
+			*ans = binsearch(a, max(beg, end - num + 1), min(end, end - num / 2 + 1) + 1, x);
+
+	}
+}
+
+
+
 void merge(int *a, int n, struct pair X, struct pair Y)
 {
 	int *aux;
-	int auxSize = X.second;
+	int auxSize = min(X.length, Y.length);
 	aux = (int*)malloc(auxSize * sizeof(int));
-	int j = X.first;
-	for (int i = 0; i < auxSize; i++)
-	{ //copying the left subarray in order not to split the task into two separate ones: mergeLeft and mergeRight
-		aux[i] = a[j];
-		j++;
+	int cnt = 0;
+	bool order = true; //copied from aux
+	if (X.length < Y.length)
+	{
+		memcpy(aux, a + X.start, X.length * sizeof(int));
+		int i = 0;
+		int j = Y.start;
+		int k = X.start;
+		while (i < auxSize && j < Y.start + Y.length)
+		{
+			if (aux[i] < a[j])
+			{
+				a[k++] = aux[i++];
+				if (order)
+					cnt++;
+				else
+				{
+					order ^= 1;
+					cnt = 1;
+				}
+			}
+			else
+			{
+				a[k++] = a[j++];
+				if (!order)
+					cnt++;
+				else
+				{
+					order ^= 1;
+					cnt = 1;
+				}
+			}
+			if (cnt >= 7)
+			{
+				if (order)
+				{
+					int finish = -100500;
+					gallop(aux, i, auxSize - 1, 1, a[j], false, &finish);
+					if (finish > i)
+					{
+						memcpy(a + k, aux + i, (finish - i) * sizeof(int));
+						k += (finish - i);
+						// fprintf(out, "Straight and order:   %lli %lli ", i, finish);
+						i = finish;
+
+					}
+				}
+				else
+				{
+					int finish = -100500;
+					gallop(a, j, Y.start + Y.length - 1, 1, aux[i], false, &finish);
+					if (finish > j)
+					{
+						int *store;
+						store = (int*)malloc((finish - j) * sizeof(int));
+						memcpy(store, a + j,
+							(finish - j) * sizeof(int));
+						memcpy(a + k, store, (finish - j) * sizeof(int));
+						k += (finish - j);
+						// fprintf(out, "Straight and !order:  %lli %lli ", i, finish);
+						j = finish;
+
+						free(store);
+					}
+				}
+
+				cnt = 0;
+			}
+		}
+		if (i < auxSize)
+		{
+			for (; k < Y.start + Y.length; k++)
+				a[k] = aux[i++];
+		}
+	}
+	else
+	{
+		memcpy(aux, a + Y.start, Y.length * sizeof(int));
+		int i = X.start + X.length - 1;
+		int j = Y.length - 1;
+		int k = Y.start + Y.length - 1;
+		while (i >= X.start && j >= 0)
+		{
+			if (aux[j] > a[i])
+			{
+				a[k--] = aux[j--];
+				if (order)
+					cnt++;
+				else
+				{
+					order ^= 1;
+					cnt = 1;
+				}
+			}
+			else
+			{
+				a[k--] = a[i--];
+				if (!order)
+					cnt++;
+				else
+				{
+					order ^= 1;
+					cnt = 1;
+				}
+			}
+			if (cnt >= 7)
+			{
+				if (order)
+				{
+					int begin = -100500;
+					gallop(aux, 0, j, 1, a[i], true, &begin);
+					if (begin < j && begin >= 0)
+					{
+						memcpy(a + k - (j - begin), aux + begin,
+							(j - begin + 1) * sizeof(int));
+						k -= (j - begin + 1);
+						// fprintf(out, "Reverse and order:  %lli %lli ", begin, j);
+						j = begin - 1;
+					}
+				}
+				else
+				{
+					int begin = -100500;
+					gallop(a, X.start, i, 1, aux[j], true, &begin);
+					if (begin < i && begin >= X.start)
+					{
+						int *store;
+						store = (int*)malloc((i - begin + 1) * sizeof(int));
+						memcpy(store, a + begin,
+							(i - begin + 1) * sizeof(int));
+						memcpy(a + k - (i - begin), store,
+							(i - begin + 1) * sizeof(int));
+						k -= (i - begin + 1);
+						// fprintf(out, "Reverse and !order:  %lli %lli ", begin, i);
+						i = begin - 1;
+						free(store);
+					}
+				}
+
+				cnt = 0;
+			}
+		}
+		if (j >= 0)
+		{
+			for (; k >= X.start; k--)
+				a[k] = aux[j--];
+		}
+
 	}
 
-	int i = 0;
-	j = 0;
-	int k = X.first;
-	bool order; //the last сopied element was from X-subarray = false, Y = true;
-	int cnt;
-	while (i < X.second && j < Y.second)
-	{
-		cnt = 0;
-		order = false;
-		if (aux[i] <= a[j + Y.first])
-		{
-			a[k] = aux[i];
-			i++;
-			if (!order) cnt++;
-			else
-			{
-				order ^= 1;
-				cnt = 1;
-			}
-		}
-		else
-		{
-			a[k] = a[j + Y.first];
-			j++;
-			if (order) cnt++;
-			else
-			{
-				order ^= 1;
-				cnt = 1;
-			}
-		}
-		k++;
-		if (cnt >= 7) //galloping mode
-		{ //if cnt numbers was copied from array, it's assumed that some more numbers will be copied from it
-			int finish;
-			if (order)
-			{
-				finish = binsearch(a, j + Y.first, Y.first + Y.second, aux[i]); //upper_bound
-				if(finish > j + Y.first)
-					memmove(a + k, a + j, (finish - j - Y.first) * sizeof(int));
-			}
-			else
-			{
-				finish = binsearch(aux, i, auxSize, a[j + Y.first]);
-				if(finish > i)
-					memmove(a + k, aux + i, (finish - i) * sizeof(int));
-			}
-		}
-	}
-	if(auxSize > i) //we can free ourselves from copying a tail of Y subarray - it's already in his place
-		memmove(a + k, aux + i, (auxSize - i) * sizeof(int));
+
 
 	free(aux);
 }
 
-void mergeAll(int *a, int n, struct pair *stack, int stackSize, int *sp)
+void mergeAll(int *a, int n, struct pair *stack, int *sp)
 { //used for merging subarrays 
-	int x, y, z, f;
-	while (*sp > 3)
+	int w, x, y, z;
+	bool wxy, xyz, yz;
+	while (true)
 	{
-		x = stack[*sp - 1].second;
-		y = stack[*sp - 2].second;
-		z = stack[*sp - 3].second;
+		if (*sp <= 1)
+			break;
+
+		z = stack[*sp - 1].length;
+		y = stack[*sp - 2].length;
+		if (*sp == 2)
+		{
+			merge(a, n, stack[*sp - 2], stack[*sp - 1]);
+			stack[*sp - 2].length += z;
+			(*sp)--;
+			break;
+		}
+
+		x = stack[*sp - 3].length;
 		if (*sp >= 4)
-			f = stack[*sp - 4].second;
+			wxy = (stack[*sp - 4].length <= x + y) ? true : false;
 		else
-			f = 1000000000;
-		if ((z <= x + y || f <= z + y) && y <= z) //left merge
+			wxy = false;
+
+		xyz = (x <= y + z) ? true : false;
+		xyz += wxy;
+		yz = (y <= z) ? true : false;
+
+		if (!xyz && !yz)
+			break;
+
+		if (xyz && !yz) //left merge
 		{
 			merge(a, n, stack[*sp - 3], stack[*sp - 2]);
-			stack[*sp - 3].second += y;
+			stack[*sp - 3].length += y;
 			stack[*sp - 2] = stack[*sp - 1];
 			(*sp)--;
 		}
 		else //right merge
 		{
 			merge(a, n, stack[*sp - 2], stack[*sp - 1]);
-			stack[*sp - 2].second += x;
+			stack[*sp - 2].length += x;
 			(*sp)--;
 		}
 	}
-	for (int i = *sp; i > 1; i--)
+
+}
+
+bool partition(int *a, int n, int *cur, int minrun, struct pair *stack, int *sp)
+{ //diving input array into sorted runs and pushing them in stack
+  //sp = StackPointer - points on the start unused cell
+	int j = getRun(a, *cur, n, minrun);
+	if (j - *cur < minrun && j < n)
 	{
-		x = stack[*sp - 1].second;
-		y = stack[*sp - 2].second;
-		merge(a, n, stack[*sp - 2], stack[*sp - 1]);
-		stack[*sp - 2].second += x;
-		(*sp)--;
+		j = min(n, *cur + minrun);
+		insertionSort(a, *cur, j);
 	}
+	stack[*sp].start = *cur;
+	stack[*sp].length = j - *cur;
+	(*sp)++;
+	*cur = j;
+
+
+	if (*cur == n)
+	{
+		while (*sp > 1)
+		{
+			merge(a, n, stack[*sp - 2], stack[*sp - 1]);
+			stack[*sp - 2].length += stack[*sp - 1].length;
+			(*sp)--;
+		}
+		free(stack);
+		return true;
+	}
+	return false;
+}
+
+inline bool is_pyramide(int *a, struct pair *stack, int *sp)
+{
+	if (*sp < 2)
+		return true;
+	int z = stack[*sp - 1].length;
+	int y = stack[*sp - 2].length;
+	return (*sp == 2 && y <= z) ? false : true;
+	int x = stack[*sp - 3].length;
+	return ((x <= y + z) || (y <= z)) ? false : true;
 }
 
 void timsort(int *a, int n)
 {
+	if (n <= 1)
+		return;
+	if (n <= 64)
+	{
+		insertionSort(a, 0, n);
+		return;
+	}
 	int minrun = getMinrun(n);
 	struct pair *stack;
-	int stackSize = n / minrun + 1; //minrun > 0 which guaranteed by if(n == 0) continue;
+	int stackSize = n / minrun + 1;
 	stack = (struct  pair*)malloc(stackSize * sizeof(struct pair));
-	int sp = 0; //sp = stackPointer
-	partition(a, n, minrun, stack, &sp);
-	mergeAll(a, n, stack, stackSize, &sp);
-	free(stack);
+	int sp = 0; //StackPointer
+	int cur = 0;
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (partition(a, n, &cur, minrun, stack, &sp))
+			return;
+
+	}
+
+	while (true)
+	{
+		if (!is_pyramide(a, stack, &sp))
+		{
+			mergeAll(a, n, stack, &sp);
+			continue;
+		}
+
+		if (partition(a, n, &cur, minrun, stack, &sp))
+			return;
+	}
+
+}
+
+long long cmp(const void *a, const void *b)
+{
+   return ( *(long long*)a - *(long long*)b );
 }
 
 int main(int argc, char* argv[])
 {
-	FILE *in, *out; 
+	
 	in = fopen("in.txt", "r");
 	out = fopen("out.txt", "w");
+	for(long long q = 500; q < 100001; q++) {
+		srand(time(NULL));
+		long long n = q;
+		// fscanf(in, "%lli", &n);
+		long long *a, *b;
+		a = (long long*)malloc(n * sizeof(long long)); 
+		b = (long long*)malloc(n * sizeof(long long)); 
+		// fprintf(out, "Unsorted array: ");
+		for(long long i = 0; i < n; i++)
+		{
+			a[i] = rand() % 1000000000;
+			b[i] = a[i];
+			// fprintf(out, "%lli ", a[i]);
+		}
 
-	srand(time(NULL));
-	int n;
-	fscanf(in, "%d", &n);
-	int *a;
-	a = (int*)malloc(n * sizeof(int)); 
+		timsort(a, n);
+		qsort (b, n, sizeof(long long), cmp);
 
-	fprintf(out, "Unsorted array: ");
-	for(int i = 0; i < n; i++)
-	{
-		a[i] = rand() % 100;
-		fprintf(out, "%d ", a[i]);
+		for(long long j = 0; j < n; j++)
+		{
+			if(b[j] != a[j])
+			
+			{
+				fprintf(out, "\n%lli %lli\n", a[j], b[j]);
+				fprintf(out, "NO\n");
+				fprintf(out, "\nUnsorted array: ");
+				for(long long i = 0; i < n; i++)
+				{
+					fprintf(out, "%lli ", a[i]);
+					if(a[i] < a[i-1])
+						fprintf(out, "\n%lli \n", a[i]);
+				}
+				fprintf(out, "\nSorted array: ");
+				for(long long i = 0; i < n; i++)
+				{
+					fprintf(out, "%lli ", b[i]);
+				}
+
+				fclose(in);
+				fclose(out);
+				return 0;
+			}
+			// fprintf(out, "%lli ", a[i]);
+		}
+
+		
+		// fprintf(out, "YES\n");
+		free(a);
+		free(b);
 	}
-
-	timsort(a, n);
-
-
-	fprintf(out, "\nSorted array:   ");
-	for(int i = 0; i < n; i++)
-		fprintf(out, "%d ", a[i]);
-
-	
-	
-	free(a);
+	fprintf(out, "YES\n");
 	fclose(in);
 	fclose(out);
+
 	return 0;
 }
